79b6ebb83775dfc758c2b8baa4d4d21ff67d2154
jonathan <jvilalop@redhat.com>
2019-07-15 11:42:09 +0200

    MIGENG-106 code prepared to use the version on the manifest

--- src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/ParamsCalculator.java
+++ src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/ParamsCalculator.java
@@ -3,29 +3,29 @@ package org.jboss.xavier.integrations.migrationanalytics.business;
 import com.jayway.jsonpath.JsonPath;
 import org.jboss.xavier.analytics.pojo.input.UploadFormInputDataModel;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 import java.util.Map;
 
 @Named("calculator")
 public class ParamsCalculator implements Calculator {
+    @Inject
+    private Environment env;
     
     @Value("${cloudforms.filter.type}")
     private String hostFilterByType;
 
-    @Value("${cloudforms.manifest.v1.cpuTotalCoresPath}")
-    String cpuTotalCoresPath;
-
-    @Value("${cloudforms.manifest.v1.cpuCoresPerSocketPath}")
-    String cpuCoresPerSocketPath;
-
-    @Value("${cloudforms.manifest.v1.totalSpacePath}")
-    String totalSpacePath;
-
-
     @Override
     public UploadFormInputDataModel calculate(String cloudFormsJson, Map<String, Object> headers) {
+        String payloadVersion = getManifestVersion(cloudFormsJson);
+        
+        String cpuTotalCoresPath = env.getProperty("cloudforms.manifest." + payloadVersion + ".cpuTotalCoresPath", env.getProperty("cloudforms.manifest.v1.cpuTotalCoresPath"));
+        String cpuCoresPerSocketPath = env.getProperty("cloudforms.manifest." + payloadVersion + ".cpuCoresPerSocketPath", env.getProperty("cloudforms.manifest.v1.cpuCoresPerSocketPath"));
+        String totalSpacePath = env.getProperty("cloudforms.manifest." + payloadVersion + ".totalSpacePath", env.getProperty("cloudforms.manifest.v1.totalSpacePath"));
+
         // Calculations
         Integer cpuTotalCores = ((List<Integer>) JsonPath.read(cloudFormsJson, cpuTotalCoresPath)).stream().mapToInt(Integer::intValue).sum();
         Integer cpuCoresPerSocket = ((List<Integer>) JsonPath.read(cloudFormsJson, cpuCoresPerSocketPath)).stream().mapToInt(Integer::intValue).sum();
@@ -47,5 +47,10 @@ public class ParamsCalculator implements Calculator {
                 sourceproductindicator, year1hypervisorpercentage,
                 year2hypervisorpercentage,
                 year3hypervisorpercentage, growthratepercentage);
+    }
+
+    // It will try to extract the version of the payload from the JSON file, falling back to v1
+    private String getManifestVersion(String cloudFormsJson) {
+        return "v1";
     }
 }
