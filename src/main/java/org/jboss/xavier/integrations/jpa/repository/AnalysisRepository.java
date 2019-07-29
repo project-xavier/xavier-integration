b4e2bb9b955db9d8360de82872d750de5e159037
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-07-29 14:24:16 +0200

    Merge branch 'master' into MIGENG-148
    # Conflicts:
    #	src/test/java/org/jboss/xavier/integrations/route/XmlRoutes_RestReportTest.java

--- src/main/java/org/jboss/xavier/integrations/jpa/repository/AnalysisRepository.java
+++ src/main/java/org/jboss/xavier/integrations/jpa/repository/AnalysisRepository.java
@@ -1,11 +1,17 @@
 package org.jboss.xavier.integrations.jpa.repository;
 
 import org.jboss.xavier.analytics.pojo.output.AnalysisModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AnalysisRepository extends JpaRepository<AnalysisModel, Long>
 {
+
+    Page<AnalysisModel> findByReportNameIgnoreCaseContaining(String filterText, Pageable pageable);
 
 }
--- src/main/java/org/jboss/xavier/integrations/jpa/service/AnalysisService.java
+++ src/main/java/org/jboss/xavier/integrations/jpa/service/AnalysisService.java
@@ -3,6 +3,10 @@ package org.jboss.xavier.integrations.jpa.service;
 import org.jboss.xavier.analytics.pojo.output.AnalysisModel;
 import org.jboss.xavier.integrations.jpa.repository.AnalysisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,4 +24,17 @@ public class AnalysisService
     {
         analysisRepository.delete(id);
     }
+
+    public Page<AnalysisModel> findReports(int page, int size)
+    {
+        Pageable pageable = new PageRequest(page, size, new Sort(Sort.Direction.DESC, "id"));
+        return analysisRepository.findAll(pageable);
+    }
+
+    public Page<AnalysisModel> findReports(String filterText, int page, int size)
+    {
+        Pageable pageable = new PageRequest(page, size, new Sort(Sort.Direction.DESC, "id"));
+        return analysisRepository.findByReportNameIgnoreCaseContaining(filterText.trim(), pageable);
+    }
+
 }
