94abb8cca95466cebff85bb00ad16e96526aa498
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-09-05 13:51:18 +0200

    change models to support OS by assessment

--- src/main/java/org/jboss/xavier/analytics/pojo/output/workload/summary/FlagAssessmentOSModel.java
+++ src/main/java/org/jboss/xavier/analytics/pojo/output/workload/summary/FlagAssessmentOSModel.java
@@ -1,14 +1,24 @@
 package org.jboss.xavier.analytics.pojo.output.workload.summary;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 
 @Entity
 public class FlagAssessmentOSModel implements java.io.Serializable {
 
+    @JsonIgnore
     @EmbeddedId
     private FlagAssessmentOSIdentityModel id;
+
+    // This comes from FlagAssessmentOSIdentityModel
+    @Column(insertable = false, updatable = false)
+    private String osName;
+
+    // This comes from FlagAssessmentOSIdentityModel
+    @Column(insertable = false, updatable = false)
+    private String assessment;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "flag_assessment_id")
@@ -29,5 +39,21 @@ public class FlagAssessmentOSModel implements java.io.Serializable {
 
     public void setFlagAssessmentModel(FlagAssessmentModel flagAssessmentModel) {
         this.flagAssessmentModel = flagAssessmentModel;
+    }
+
+    public String getOsName() {
+        return osName;
+    }
+
+    public void setOsName(String osName) {
+        this.osName = osName;
+    }
+
+    public String getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        this.assessment = assessment;
     }
 }
