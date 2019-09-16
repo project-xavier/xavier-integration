df5ad73991eb7a81994ca82212e5d3368a61dec0
jonathan <jvilalop@redhat.com>
2019-09-16 22:57:36 +0200

    MIGENG-235 Fixed properties handling storing expanded versions Added test values, added test with properties file

--- src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/versioning/VersionService.java
+++ src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/versioning/VersionService.java
@@ -11,7 +11,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
@@ -23,7 +23,7 @@ public class VersionService {
 
     @Getter
     @Setter
-    private List<String> properties;
+    private Map<String, String> properties;
 
     @PostConstruct
     public void init() {
@@ -33,32 +33,41 @@ public class VersionService {
     public String getPropertyWithFallbackVersion(String payloadVersion, String path) {
         String fallbackVersion = getFallbackVersionPath(payloadVersion, path);
 
-        return env.getProperty("cloudforms.manifest." + payloadVersion + path, env.getProperty("cloudforms.manifest.v" + fallbackVersion + path));
+        return properties.get("cloudforms.manifest.v" + fallbackVersion + "." + path);
     }
 
     public String getFallbackVersionPath(String payloadVersion, String path) {
+        ManifestPathVersion fallbackversion = properties.keySet()
+                .stream()
+                .filter(e -> e.matches("cloudforms.manifest.v[0-9_]*." + path))
+                .map(e -> e.split("\\.")[2])
+                .map(e -> e.replace("v", ""))
+                .map(this::expandVersion)
+                .map(ManifestPathVersion::new)
+                .sorted(Comparator.reverseOrder())
+                .filter(e -> isVersionEqualOrLower(payloadVersion, e))
+                .findFirst()
+                .orElse(new ManifestPathVersion("0_0_0"));
 
-        return properties
-                    .stream()
-                    .filter(e -> e.matches("cloudforms\\.manifest\\.v[0-9_]*\\." + path))
-                    .map(e -> e.split("\\.")[2])
-                    .map(e -> e.replace("v", ""))
-                    .map(this::expandVersion)
-                    .map(ManifestPathVersion::new)
-                    .sorted(Comparator.reverseOrder())
-                    .filter(e -> e.compareTo(expandVersion(payloadVersion)) <= 0 )
-                    .findFirst()
-                    .map(ManifestPathVersion::getVersion)
-                    .orElse("v0_0_0");
+        return fallbackversion.getVersion();
     }
 
-    private List<String> getAllProperties() {
+    private boolean isVersionEqualOrLower(String payloadVersion, ManifestPathVersion e) {
+        return e.compareTo(expandVersion(payloadVersion)) <= 0;
+    }
+
+    private Map<String, String> getAllProperties() {
         return StreamSupport.stream(((AbstractEnvironment) env).getPropertySources().spliterator(), false)
                     .filter(ps -> ps instanceof EnumerablePropertySource)
                     .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
                     .flatMap(Arrays::stream)
-                    .filter(e -> e.startsWith("cloudforms.manifest."))
-                    .collect(Collectors.toList());
+                .filter(e -> e.matches("cloudforms.manifest.v[0-9_]*.*"))
+                .collect(Collectors.toMap( this::getKey, e-> env.getProperty(e)));
+    }
+
+    private String getKey(String property) {
+        String[] elements = property.split("\\.");
+        return property.replace(elements[2], "v" + expandVersion(elements[2].replace("v", "")));
     }
 
     public String expandVersion(String e) {
--- src/test/java/org/jboss/xavier/integrations/migrationanalytics/business/VersionServiceTest.java
+++ src/test/java/org/jboss/xavier/integrations/migrationanalytics/business/VersionServiceTest.java
@@ -1,17 +1,35 @@
 package org.jboss.xavier.integrations.migrationanalytics.business;
 
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.UseAdviceWith;
+import org.jboss.xavier.Application;
 import org.jboss.xavier.integrations.migrationanalytics.business.versioning.ManifestPathVersion;
 import org.jboss.xavier.integrations.migrationanalytics.business.versioning.VersionService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
+import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
+@RunWith(CamelSpringBootRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringBootTest(classes = {Application.class})
+@UseAdviceWith // Disables automatic start of Camel context
+@ActiveProfiles("test")
 public class VersionServiceTest {
+    @Inject
+    VersionService versionServiceBean;
+
     @Test
     public void compareVersion_FewVersionNumbersGiven_ReturnsCorrectComparisson() {
       assertThat(new ManifestPathVersion("1_0_0").compareTo("1_0_0")).isEqualTo(0);
@@ -27,7 +45,6 @@ public class VersionServiceTest {
 
     @Test
     public void sortVersions_FewVersionNumbersGiven_ReturnsCorrectOrder() {
-        VersionService versionService = new VersionService();
         List<String> mylist = Arrays.asList("2_1_1","1_0_0", "1_8_2","1_1_0", "2_2_0");
         List<ManifestPathVersion> mySortedList = mylist.stream().map(ManifestPathVersion::new).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         assertThat(mySortedList.get(mySortedList.size() -1)).isEqualTo(new ManifestPathVersion("1_0_0"));
@@ -45,17 +62,28 @@ public class VersionServiceTest {
     @Test
     public void getFallbackVersion_FewShortAndLongVersions_ReturnsClosestHighestVersions() {
         VersionService versionService = new VersionService();
-        versionService.setProperties(Arrays.asList("cloudforms.manifest.v1_0.primero",
-                                                   "cloudforms.manifest.v1_1.primero",
-                                                   "cloudforms.manifest.v1_1_2.primero",
-                                                   "cloudforms.manifest.v2.primero",
-                                                   "cloudforms.manifest.v1_2.primero",
-                                                   "cloudforms.manifest.v2_2.primero",
-                                                   "cloudforms.manifest.v2_1_3.primero",
-                                                   "cloudforms.manifest.v3_1_3.segundo"));
+        Map<String,String> properties = new HashMap<>();
+        properties.put("cloudforms.manifest.v1_0.primero", "val1");
+        properties.put("cloudforms.manifest.v1_1.primero", "val2");
+        properties.put("cloudforms.manifest.v1_1_2.primero","val3");
+        properties.put("cloudforms.manifest.v2.primero","val4");
+        properties.put("cloudforms.manifest.v1_2.primero","val5");
+        properties.put("cloudforms.manifest.v2_2.primero","val6");
+        properties.put("cloudforms.manifest.v2_1_3.primero","val7");
+        properties.put("cloudforms.manifest.v3_1_3.segundo","val8");
+        versionService.setProperties(properties);
         assertThat(versionService.getFallbackVersionPath("1_0_0", "primero")).isEqualToIgnoringCase("1_0_0");
         assertThat(versionService.getFallbackVersionPath("1_3", "primero")).isEqualToIgnoringCase("1_2_0");
     }
+
+    @Test
+    public void getFallbackVersion_PropertiesFileGive_ReturnClosesHighestVersionPaths() {
+        assertThat(versionServiceBean.getPropertyWithFallbackVersion("10_3_0", "vmworkloadinventory.providerPath")).isEqualToIgnoringCase("providerPath_v10_2_3");
+        assertThat(versionServiceBean.getPropertyWithFallbackVersion("0_2_0", "hypervisor.cpuTotalCoresPath")).isEqualToIgnoringCase("cpu_total_cores");
+        assertThat(versionServiceBean.getPropertyWithFallbackVersion("20_0_80", "vmworkloadinventory.providerPath")).isEqualToIgnoringCase("providerPath_v20");
+        assertThat(versionServiceBean.getPropertyWithFallbackVersion("30_0_80", "vmworkloadinventory.providerPath")).isEqualToIgnoringCase("providerPath_v20_1_2");
+    }
+
 
 
 }
