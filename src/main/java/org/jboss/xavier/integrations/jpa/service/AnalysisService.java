1ab1ba7b694a847309545b59fbae3000b9432c80
jonathan <jvilalop@redhat.com>
2019-09-26 21:22:20 +0200

    MIGENG-253 Adding unescaping capabilities

--- src/main/java/org/jboss/xavier/integrations/jpa/service/AnalysisService.java
+++ src/main/java/org/jboss/xavier/integrations/jpa/service/AnalysisService.java
@@ -1,5 +1,6 @@
 package org.jboss.xavier.integrations.jpa.service;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.jboss.xavier.analytics.pojo.output.AnalysisModel;
 import org.jboss.xavier.analytics.pojo.output.InitialSavingsEstimationReportModel;
 import org.jboss.xavier.analytics.pojo.output.workload.inventory.WorkloadInventoryReportModel;
@@ -123,5 +124,9 @@ public class AnalysisService
         analysisModel.setPayloadURL(payloadURL);
         analysisModel.setLastUpdate(new Date());
         analysisRepository.save(analysisModel);
+    }
+
+    public String unescape(String htmlString) {
+        return StringEscapeUtils.unescapeHtml4(htmlString);
     }
 }
