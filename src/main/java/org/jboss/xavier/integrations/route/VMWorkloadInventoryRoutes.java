d1b0fb94693d26d5a15bde8551516b6db8ca7ac2
jonathan <jvilalop@redhat.com>
2019-10-01 15:02:05 +0200

    MIGENG-243 removed doTry that were still in the code

--- src/main/java/org/jboss/xavier/integrations/route/VMWorkloadInventoryRoutes.java
+++ src/main/java/org/jboss/xavier/integrations/route/VMWorkloadInventoryRoutes.java
@@ -33,47 +33,28 @@ public class VMWorkloadInventoryRoutes extends RouteBuilder {
         from("direct:calculate-vmworkloadinventory").id("calculate-vmworkloadinventory")
             .transform().method(VMWorkloadInventoryCalculator.class, "calculate(${body}, ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}})")
             .split(body()).parallelProcessing(parallel).aggregationStrategy(new WorkloadInventoryReportModelAggregationStrategy())
-            .to("direct:vm-workload-inventory")
+                .to("direct:vm-workload-inventory")
             .end()
-            .process(exchange -> {
-                analysisService.addWorkloadInventoryReportModels(exchange.getIn().getBody(List.class),
-                        Long.parseLong(exchange.getIn().getHeader(MainRouteBuilder.MA_METADATA, Map.class).get(MainRouteBuilder.ANALYSIS_ID).toString()));
-            });
+            .process(exchange -> analysisService.addWorkloadInventoryReportModels(exchange.getIn().getBody(List.class),
+                    Long.parseLong(exchange.getIn().getHeader(MainRouteBuilder.MA_METADATA, Map.class).get(MainRouteBuilder.ANALYSIS_ID).toString())));
 
         from ("direct:vm-workload-inventory").id("extract-vmworkloadinventory")
-            .doTry()
-                .setHeader(MainRouteBuilder.ANALYSIS_ID, simple("${body." + MainRouteBuilder.ANALYSIS_ID + "}", String.class))
-                .transform().method("decisionServerHelper", "generateCommands(${body}, \"GetWorkloadInventoryReports\", \"WorkloadInventoryKSession0\")")
-                .to("direct:decisionserver").id("workload-decisionserver")
-                .transform().method("decisionServerHelper", "extractWorkloadInventoryReportModel")
-            .endDoTry()
-            .doCatch(Exception.class)
-                .to("log:error?showCaughtException=true&showStackTrace=true")
-                .transform().method("analysisService", "updateStatus(\"FAILED\", ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.ANALYSIS_ID}})")
-                .stop()
-            .end();
+            .setHeader(MainRouteBuilder.ANALYSIS_ID, simple("${body." + MainRouteBuilder.ANALYSIS_ID + "}", String.class))
+            .transform().method("decisionServerHelper", "generateCommands(${body}, \"GetWorkloadInventoryReports\", \"WorkloadInventoryKSession0\")")
+            .to("direct:decisionserver").id("workload-decisionserver")
+            .transform().method("decisionServerHelper", "extractWorkloadInventoryReportModel");
 
         from("direct:flags-shared-disks").id("flags-shared-disks")
-            .doTry()
-                .transform().method(FlagSharedDisksCalculator.class, "calculate(${body}, ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}})")
-                .process(exchange -> {
-                    Set<String> vmNamesWithSharedDisk = exchange.getIn().getBody(Set.class);
-                    List<WorkloadInventoryReportModel> workloadInventoryReportModels = workloadInventoryReportService.findByAnalysisOwnerAndAnalysisId(
-                            exchange.getIn().getHeader(MainRouteBuilder.USERNAME, String.class),
-                            Long.parseLong(exchange.getIn().getHeader(MainRouteBuilder.MA_METADATA, Map.class).get(MainRouteBuilder.ANALYSIS_ID).toString()));
-                    List<WorkloadInventoryReportModel> workloadInventoryReportModelsToUpdate = workloadInventoryReportModels.stream()
-                        .filter(workloadInventoryReportModel -> vmNamesWithSharedDisk.contains(workloadInventoryReportModel.getVmName()))
-                        .map(workloadInventoryReportModel -> {
-                            workloadInventoryReportModel.addFlagIMS("Shared Disk");
-                            return workloadInventoryReportModel;
-                        }).collect(Collectors.toList());
-                    workloadInventoryReportService.saveAll(workloadInventoryReportModelsToUpdate);
-                })
-            .endDoTry()
-            .doCatch(Exception.class)
-                .to("log:error?showCaughtException=true&showStackTrace=true")
-                .transform().method("analysisService", "updateStatus(\"FAILED\", ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}[" + MainRouteBuilder.ANALYSIS_ID + "]}")
-                .stop()
-            .end();
+            .transform().method(FlagSharedDisksCalculator.class, "calculate(${body}, ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}})")
+            .process(exchange -> {
+                Set<String> vmNamesWithSharedDisk = exchange.getIn().getBody(Set.class);
+                List<WorkloadInventoryReportModel> workloadInventoryReportModels = workloadInventoryReportService.findByAnalysisOwnerAndAnalysisId(
+                        exchange.getIn().getHeader(MainRouteBuilder.USERNAME, String.class),
+                        Long.parseLong(exchange.getIn().getHeader(MainRouteBuilder.MA_METADATA, Map.class).get(MainRouteBuilder.ANALYSIS_ID).toString()));
+                List<WorkloadInventoryReportModel> workloadInventoryReportModelsToUpdate = workloadInventoryReportModels.stream()
+                    .filter(workloadInventoryReportModel -> vmNamesWithSharedDisk.contains(workloadInventoryReportModel.getVmName()))
+                    .peek(workloadInventoryReportModel -> workloadInventoryReportModel.addFlagIMS("Shared Disk")).collect(Collectors.toList());
+                workloadInventoryReportService.saveAll(workloadInventoryReportModelsToUpdate);
+            });
     }
 }
