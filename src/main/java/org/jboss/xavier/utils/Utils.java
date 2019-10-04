133413368c554d651a31c3b48a9c886d44130a8e
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-10-04 09:13:28 +0200

    remove duplicate method

--- src/main/java/org/jboss/xavier/utils/Utils.java
+++ src/main/java/org/jboss/xavier/utils/Utils.java
@@ -1,13 +1,8 @@
 package org.jboss.xavier.utils;
 
-import java.util.Date;
 import java.util.UUID;
 
 public class Utils {
-
-    public Date getUnixEpochDate() {
-        return new Date(0L);
-    }
 
     public String generateUUID() {
         return UUID.randomUUID().toString();
