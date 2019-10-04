12fd0aeea089bc5d007580449aefc84cfa30916d
jonathan <jvilalop@redhat.com>
2019-10-04 16:53:39 +0200

    MIGENG-243 [PR] fix typo and add null check before marking as failed

--- src/main/java/org/jboss/xavier/integrations/route/RouteBuilderExceptionHandler.java
+++ src/main/java/org/jboss/xavier/integrations/route/RouteBuilderExceptionHandler.java
@@ -2,6 +2,7 @@ package org.jboss.xavier.integrations.route;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.jboss.xavier.integrations.jpa.service.AnalysisService;
 
 import javax.inject.Inject;
@@ -29,9 +30,11 @@ public abstract class RouteBuilderExceptionHandler extends RouteBuilder {
             if (analysisId.isEmpty()) {
                 analysisId = (String) e.getIn().getHeader(MA_METADATA, Map.class).get(ANALYSIS_ID);
             }
-            analysisService.markAsFailedIfNotCreated(Long.parseLong(analysisId));
+            if (StringUtils.isNotEmpty(analysisId)) {
+                analysisService.markAsFailedIfNotCreated(Long.parseLong(analysisId));
+            }
         } catch (Exception ex) {
-            log.error("Exception ocurred while marking the Analysis as failed.", ex);
+            log.error("Exception occurred while marking the Analysis as failed.", ex);
         }
     }
 }
