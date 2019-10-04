2b3c26d8c65ce9da8531feef9d6af1cbe775af2d
jonathan <jvilalop@redhat.com>
2019-10-04 17:33:45 +0200

    MIGENG-243 removed duplicated code

--- src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/VMWorkloadInventoryCalculator.java
+++ src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/VMWorkloadInventoryCalculator.java
@@ -12,8 +12,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Component
@@ -62,69 +60,5 @@ public class VMWorkloadInventoryCalculator extends AbstractVMWorkloadInventoryCa
         model.setAnalysisId(analysisId);
 
         return model;
-    }
-
-    private Map<String, String> readMapValuesFromExpandedEnvVarPath(String envVarPath, Map vmStructMap, String keyfield, String valuefield) {
-        String expandParamsInPath = getExpandedPath(envVarPath, vmStructMap);
-        Map<String,String> files = new HashMap<>();
-        try {
-            List<List<Map>> value = jsonParsed.read(expandParamsInPath);
-            value.stream().flatMap(Collection::stream).collect(Collectors.toList()).forEach(e-> files.put((String) e.get(keyfield), (String) e.get(valuefield)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return files;
-    }
-
-    private <T> T readValueFromExpandedEnvVarPath(String envVarPath, Map vmStructMap, Class type) {
-        String expandParamsInPath = getExpandedPath(envVarPath, vmStructMap);
-
-        Object value = jsonParsed.read(expandParamsInPath);
-        if (value instanceof Collection) {
-            value = ((List<T>) value).get(0);
-        }
-        if (Long.class.isAssignableFrom(type)) {
-            value = Long.valueOf(((Number) value).longValue());
-        } else if (Integer.class.isAssignableFrom(type)) {
-            value = Integer.valueOf(((Number) value).intValue());
-        }
-        return (T) value;
-    }
-
-    private <T> T readValueFromExpandedEnvVarPath(String envVarPath, Map vmStructMap) {
-        return readValueFromExpandedEnvVarPath(envVarPath, vmStructMap, Object.class);
-    }
-
-    private <T> List<T> readListValuesFromExpandedEnvVarPath(String envVarPath, Map vmStructMap) {
-        String expandParamsInPath = getExpandedPath(envVarPath, vmStructMap);
-
-        Object value = jsonParsed.read(expandParamsInPath);
-        if (value instanceof Collection) {
-            return new ArrayList<>((List<T>) value);
-        } else {
-            return Collections.singletonList((T) value);
-        }
-    }
-
-    private String getExpandedPath(String envVarPath, Map vmStructMap) {
-        String envVarPathWithExpandedVersion = expandVersionInExpression(envVarPath);
-        String path = env.getProperty(envVarPathWithExpandedVersion);
-        return expandParamsInPath(path, vmStructMap);
-    }
-
-    private String expandVersionInExpression(String path) {
-        return path.replace("{version}", manifestVersion);
-    }
-
-    private String expandParamsInPath(String path, Map vmStructMap) {
-        Pattern p = Pattern.compile("\\{[a-zA-Z1-9_]+\\}");
-        Matcher m = p.matcher(path);
-        while (m.find() && vmStructMap != null) {
-            String key = m.group().substring(1, m.group().length() - 1);
-            String value = vmStructMap.containsKey(key) ? vmStructMap.get(key).toString() : "";
-            path = path.replace(m.group(), value);
-        }
-
-        return path;
     }
 }
