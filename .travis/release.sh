2012383df9f98b8d66c50dabe56d4da9c7318c16
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2019-12-12 17:42:24 +0100

    pull fuse image

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -n "$TRAVIS_TAG" ]; then
+  # We can choose eithe follow this block of commands to download the image without authentication in Red Hat Registry
+  # Or we can create a Registry Service Account and login here
   docker pull registry.access.redhat.com/fuse7/fuse-java-openshift
   docker tag registry.access.redhat.com/fuse7/fuse-java-openshift:latest fuse7/fuse-java-openshift:latest
-  docker tag registry.access.redhat.com/fuse7/fuse-java-openshift:latest fuse7/fuse-java-openshift:1.3
+  docker tag registry.access.redhat.com/fuse7/fuse-java-openshift:latest fuse7/fuse-java-openshift:1.3 # fabric8 use this specific version
 
   # Build image
   mvn fabric8:build -Dfabric8.mode=kubernetes
-
-  docker images
 
   # Tag Docker image
   docker tag xavier/xavier-integration:"$TRAVIS_TAG" projectxavier/xavier-integration:latest
