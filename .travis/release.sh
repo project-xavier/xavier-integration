83c9a8d780355bf8d7fa2e084dd3af4e5111e9d7
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2019-12-12 16:39:00 +0100

    change script execution

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,9 +1,13 @@
-mvn fabric8:build -Dfabric8.mode=kubernetes
+#!/bin/bash
+if [ "$TRAVIS_TAG" != "" ]; then
+  # Build image
+  mvn fabric8:build -Dfabric8.mode=kubernetes
 
-# Tag Docker image
-docker tag xavier/xavier-integration xavier/xavier-integration:latest
-docker tag xavier/xavier-integration xavier/xavier-integration:"$TRAVIS_TAG"
+  # Tag Docker image
+  docker tag xavier/xavier-integration xavier/xavier-integration:latest
+  docker tag xavier/xavier-integration xavier/xavier-integration:"$TRAVIS_TAG"
 
-# Push our image to the Image Registry
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-docker push xavier/xavier-integration
+  # Push our image to the Image Registry
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
+  docker push xavier/xavier-integration
+fi
