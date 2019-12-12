544f057b7dcd624c1584550a97c62a0e71d5cc3f
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2019-12-12 17:43:41 +0100

    change release organization

--- release.sh
+++ release.sh
@@ -11,15 +11,15 @@ mvn versions:set -DnewVersion="$releaseVersion"
 
 git add .
 git commit -m "Release $releaseVersion"
-git push "https://$username:$password@github.com/carlosthe19916/xavier-integration.git"
+git push "https://$username:$password@github.com/project-xavier/xavier-integration.git"
 
 # Create tag and push
 git tag "$releaseVersion"
-git push "https://$username:$password@github.com/carlosthe19916/xavier-integration.git" --tags
+git push "https://$username:$password@github.com/project-xavier/xavier-integration.git" --tags
 
 # Create next snapshot
 mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
 
 git add .
 git commit -m "Prepare next release $nextVersion-SNAPSHOT"
-git push "https://$username:$password@github.com/carlosthe19916/xavier-integration.git"
+git push "https://$username:$password@github.com/project-xavier/xavier-integration.git"
