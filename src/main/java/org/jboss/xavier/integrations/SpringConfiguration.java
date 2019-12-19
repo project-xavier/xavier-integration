a5fb54522df7602ec9bea4a9374db82516a3ca16
jonathan <jvilalop@redhat.com>
2019-12-19 12:30:26 +0100

    MIGENG-365 as we are not specifying profile, moved the regular SpringConfiguration to any profile except test

--- src/main/java/org/jboss/xavier/integrations/SpringConfiguration.java
+++ src/main/java/org/jboss/xavier/integrations/SpringConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"prod", "dev"})
+@Profile("!test")
 public class SpringConfiguration {
     @Value("${S3_ACCESS_KEY_ID}")
     String key;
