953a97c48b2c9c9975197fb682384f981c215c80
Jonathan Vila <jvilalop@redhat.com>
2020-01-22 13:01:34 +0100

    Migeng 327 avoid crashes based on issues use cases (#68)
    * MIGENG-327 added try-catch to the list of values json reader
    
    * MIGENG-327 added null check on the vmStructMap
    
    * end2end adding conditionals to avoid division by zero
    
    * MIGENG-327 added test to cover null and 0 on calculation of hypervisors
    
    * MIGENG-327 added test to cover missing attributes on FlagSharedDisksCalculator
    
    * MIGENG-327 fixed test on missing atts for FlagSharedDisksCalculator
    
    * MIGENG-327 added test to cover missing attributes on VMWorkloadInventoryCalculator
    added abstract clause to AbstractVMWorkloadInventoryCalculator
    
    * MIGENG-327 added test for EndToEnd with a file with missing attributes
    
    * MIGENG-327 changed the approach to not set 0 as hypervisors in order to avoid division By Zero
    
    * MIGENG-327 added individual test files with each use case
    
    * MIGENG-327 avoided issue with Localstack and json files being uploaded
    
    * MIGENG-327 reduced output on Exceptions reading from JSON
    Added method to avoid ClassCastException on reading from JSON
    Added conditional to avoid setting a default hasRdmDisk
    
    * MIGENG-327 upgraded Testcontainers version
    
    * MIGENG-327 Created Handler for the issues recording
    
    * MIGENG-327 Introduced the errors in the 3 test files for the issues use cases
    
    * MIGENG-327 Introduced the issues recorder in the calculators
    
    * MIGENG-327 Fixed calculator test method call adding analysisId
    
    * MIGENG-327 Added values tests on End2End verifying the reports on the 3 issues use cases
    
    * MIGENG-327 Refactored the record method to include entity ( VM, HOST, ... )
    
    * MIGENG-327 Refactored End2End report calls
    
    * MIGENG-327 added asserts on the total of VMs
    modified asserts on "No ..... defined"
    
    * MIGENG-327 [PR] refactored variable name

--- src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/ParamsCalculator.java
+++ src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/ParamsCalculator.java
@@ -4,6 +4,7 @@ import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
 import org.apache.commons.lang3.StringUtils;
 import org.jboss.xavier.analytics.pojo.input.UploadFormInputDataModel;
+import org.jboss.xavier.integrations.migrationanalytics.business.issuehandling.AnalysisIssuesHandler;
 import org.jboss.xavier.integrations.migrationanalytics.business.versioning.ManifestVersionService;
 import org.jboss.xavier.integrations.route.RouteBuilderExceptionHandler;
 
@@ -11,17 +12,38 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Named("calculator")
 public class ParamsCalculator implements Calculator<UploadFormInputDataModel> {
-    @Inject
-    ManifestVersionService manifestVersionService;
+    private final ManifestVersionService manifestVersionService;
 
-    private static Integer calculateHypervisors(Object e, String cpuTotalCoresPath, String cpuCoresPerSocketPath) {
-        Map mapa = (Map) e;
-        Integer cputotalcores = (Integer) mapa.get(cpuTotalCoresPath);
-        Integer cpucorespersocket = (Integer) mapa.get(cpuCoresPerSocketPath);
-        return (int) Math.ceil(cputotalcores / (cpucorespersocket * 2.0));
+    @Inject
+    AnalysisIssuesHandler analysisIssuesHandler;
+
+    @Inject
+    public ParamsCalculator(ManifestVersionService manifestVersionService) {
+        this.manifestVersionService = manifestVersionService;
+    }
+
+    public Integer calculateHypervisors(Map valuesMap, String cpuTotalCoresPath, String cpuCoresPerSocketPath, String analysisId) {
+        Integer cputotalcores = getMapValueAvoidClassCastException(valuesMap, cpuTotalCoresPath, Integer.class);
+        Integer cpucorespersocket = getMapValueAvoidClassCastException(valuesMap, cpuCoresPerSocketPath, Integer.class);
+        if (cputotalcores != null && cpucorespersocket != null && cpucorespersocket > 0) {
+            return (int) Math.ceil(cputotalcores / (cpucorespersocket * 2.0));
+        } else {
+            analysisIssuesHandler.record(analysisId, "HOST", valuesMap.get("name").toString(), cpuCoresPerSocketPath, "Invalid values to calculate Hypervisors number");
+            return null;
+        }
+    }
+
+    public <T> T getMapValueAvoidClassCastException(Map valuesMap, String key, Class<T> expectedClass) {
+        Object mapValue = (T) valuesMap.get(key);
+        if (mapValue != null && mapValue.getClass().isAssignableFrom(expectedClass)) {
+            return (T) mapValue;
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -34,7 +56,12 @@ public class ParamsCalculator implements Calculator<UploadFormInputDataModel> {
         String totalSpacePath = manifestVersionService.getPropertyWithFallbackVersion(payloadVersion, "totalSpacePath");
 
         // Calculations
-        Integer numberofhypervisors = ((JSONArray) JsonPath.read(cloudFormsJson, hypervisorPath)).stream().map(e -> calculateHypervisors(e, cpuTotalCoresPath, cpuCoresPerSocketPath)).mapToInt(Integer::intValue).sum();
+        Integer numberofhypervisors = ((JSONArray) JsonPath.read(cloudFormsJson, hypervisorPath))
+                .stream()
+                .map(e -> calculateHypervisors((Map) e, cpuTotalCoresPath, cpuCoresPerSocketPath, headers.get(RouteBuilderExceptionHandler.ANALYSIS_ID).toString()))
+                .filter(Objects::nonNull)
+                .mapToInt(Integer::intValue)
+                .sum();
         Long totalspace = ((List<Number>) JsonPath.parse(cloudFormsJson).read(totalSpacePath)).stream().mapToLong(Number::longValue).sum();
 
 
