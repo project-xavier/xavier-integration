933d2d84047dd09dfd95307711d95be792773183
jonathan <jvilalop@redhat.com>
2020-01-22 15:55:20 +0100

    hotfix-kafka-autooffset set autoOffsetReset to latest

--- src/main/java/org/jboss/xavier/integrations/route/MainRouteBuilder.java
+++ src/main/java/org/jboss/xavier/integrations/route/MainRouteBuilder.java
@@ -121,7 +121,7 @@ public class MainRouteBuilder extends RouteBuilderExceptionHandler {
                         .throwException(org.apache.commons.httpclient.HttpException.class, "Unsuccessful response from Insights Upload Service")
                 .end();
 
-        from("kafka:" + kafkaHost + "?topic={{insights.kafka.upload.topic}}&brokers=" + kafkaHost + "&autoOffsetReset=earliest&autoCommitEnable=true")
+        from("kafka:" + kafkaHost + "?topic={{insights.kafka.upload.topic}}&brokers=" + kafkaHost + "&autoOffsetReset=latest&autoCommitEnable=true")
                 .routeId("kafka-upload-message")
                 .unmarshal().json(JsonLibrary.Jackson, FilePersistedNotification.class)
                 .filter(simple("'{{insights.service}}' == ${body.getService}"))
