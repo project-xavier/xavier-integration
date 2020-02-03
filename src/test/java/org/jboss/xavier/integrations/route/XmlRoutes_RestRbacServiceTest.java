fee1de34791537ab8ee7e5995de6aac862ade939
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-03 09:36:01 +0100

    Remove greetings route and add /user tests

--- src/test/java/org/jboss/xavier/integrations/route/XmlRoutes_RestRbacServiceTest.java
+++ src/test/java/org/jboss/xavier/integrations/route/XmlRoutes_RestRbacServiceTest.java
@@ -1,7 +1,13 @@
 package org.jboss.xavier.integrations.route;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.jboss.xavier.Application;
-import org.jboss.xavier.integrations.jpa.service.GreetingsService;
+import org.jboss.xavier.integrations.jpa.service.UserService;
+import org.jboss.xavier.integrations.rbac.Acl;
+import org.jboss.xavier.integrations.rbac.RbacResponse;
 import org.jboss.xavier.integrations.util.TestUtil;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,10 +21,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,7 +37,7 @@ public class XmlRoutes_RestRbacServiceTest extends XavierCamelTest {
     String camel_context;
 
     @SpyBean
-    private GreetingsService greetingsService;
+    private UserService userService;
 
     @Before
     public void setup() {
@@ -38,7 +45,7 @@ public class XmlRoutes_RestRbacServiceTest extends XavierCamelTest {
     }
 
     @Test
-    public void xmlRouteBuilder_RestGreetings() throws Exception {
+    public void xmlRouteBuilder_GivenAllAccess_shouldReturnUser() throws Exception {
         //Given
 
 
@@ -46,28 +53,63 @@ public class XmlRoutes_RestRbacServiceTest extends XavierCamelTest {
         camelContext.start();
         TestUtil.mockRBACResponse(camelContext);
         TestUtil.startUsernameRoutes(camelContext);
-        camelContext.startRoute("my-endpoint-route");
-
-        Map<String, String> variables = new HashMap<>();
-        String queryParamKey = "queryParamKey";
-        String queryParamValue = "queryParamValue";
-        variables.put("queryParamKey", queryParamValue);
+        camelContext.startRoute("get-user-info");
 
         HttpHeaders headers = new HttpHeaders();
         headers.set(TestUtil.HEADER_RH_IDENTITY, TestUtil.getBase64RHIdentity());
 
-        Map<String, String> body = new HashMap<>();
-        body.put("key1", "value1");
-        body.put("key2", "value2");
-
-        HttpEntity<Map> entity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.exchange(camel_context + "/greetings?queryParamKey={queryParamKey}", HttpMethod.POST, entity, String.class, variables);
+        HttpEntity<Map> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(camel_context + "/user", HttpMethod.GET, entity, String.class);
 
         //Then
-        verify(greetingsService).greetings("mrizzi@redhat.com", variables.get(queryParamKey), body);
+        verify(userService).findUser("mrizzi@redhat.com");
         assertThat(response).isNotNull();
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
-        assertThat(response.getBody()).contains("\"Greetings to username=" + "mrizzi@redhat.com" + " queryValue=" + queryParamValue + " body=" + body + "\"");
+        assertThat(response.getBody()).contains("{\"firstTimeCreatingReports\":true}");
+
+        camelContext.stop();
+    }
+
+    @Test
+    public void xmlRouteBuilder_GivenEmptyAccess_shouldReturnForbidden() throws Exception {
+        //Given
+
+
+        //When
+        camelContext.start();
+        camelContext.getRouteDefinition("fetch-rbac-user-access").adviceWith(camelContext, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveById("fetch-rbac-user-access-endpoint").replace()
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("200"))
+                        .setBody(exchange -> {
+                            RbacResponse rbacResponse = new RbacResponse(
+                                    new RbacResponse.Meta(1, 10, 0),
+                                    new RbacResponse.Links(null, null, null, null),
+                                    Collections.emptyList()
+                            );
+                            try {
+                                return new ObjectMapper().writeValueAsString(rbacResponse);
+                            } catch (JsonProcessingException e) {
+                                e.printStackTrace();
+                                throw new IllegalStateException(e);
+                            }
+                        });
+            }
+        });
+        TestUtil.startUsernameRoutes(camelContext);
+        camelContext.startRoute("get-user-info");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(TestUtil.HEADER_RH_IDENTITY, TestUtil.getBase64RHIdentity());
+
+        HttpEntity<Map> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(camel_context + "/user", HttpMethod.GET, entity, String.class);
+
+        //Then
+        verify(userService, never()).findUser("mrizzi@redhat.com");
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCodeValue()).isEqualTo(403);
 
         camelContext.stop();
     }
