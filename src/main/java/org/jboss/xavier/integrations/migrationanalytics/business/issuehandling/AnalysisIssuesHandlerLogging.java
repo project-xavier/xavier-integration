9d103484d06ef54ff2e0eb1a307bb715fdefb1a8
jonathan <jvilalop@redhat.com>
2020-02-12 12:42:45 +0100

    MIGENG-412 : added toggle to enable/disable log of data integrity issues

--- src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/issuehandling/AnalysisIssuesHandlerLogging.java
+++ src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/issuehandling/AnalysisIssuesHandlerLogging.java
@@ -1,14 +1,19 @@
 package org.jboss.xavier.integrations.migrationanalytics.business.issuehandling;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class AnalysisIssuesHandlerLogging implements AnalysisIssuesHandler {
+    @Value("${analysis.dataintegrity.log:true}")
+    boolean analysisDataIntegrityLog;
 
     @Override
     public void record(String analysisId, String entity, String entityName, String jsonPath, String message) {
-        log.warn("Exception on {} [{}] reading value from JSON [{}}] : {}" , entity, entityName, jsonPath, message);
+        if (analysisDataIntegrityLog) {
+            log.warn("Exception on {} [{}] reading value from JSON [{}}] : {}", entity, entityName, jsonPath, message);
+        }
     }
 }
