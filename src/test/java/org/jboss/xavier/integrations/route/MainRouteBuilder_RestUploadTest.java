366ebedabf1534599afef0b1ff3907d130f05982
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-09 18:41:59 +0100

    remove no needed changes

--- src/test/java/org/jboss/xavier/integrations/route/MainRouteBuilder_RestUploadTest.java
+++ src/test/java/org/jboss/xavier/integrations/route/MainRouteBuilder_RestUploadTest.java
@@ -38,7 +38,7 @@ public class MainRouteBuilder_RestUploadTest extends XavierCamelTest {
     public void setup() {
         camel_context = camel_context.substring(0, camel_context.indexOf("*"));
     }
-
+   
     @Test
     public void mainRouteBuilder_routeRestUpload_ContentGiven_ShouldUpload() throws Exception {
         //Given
--- src/test/java/org/jboss/xavier/integrations/route/dataformat/CustomizedMultipartDataFormatTest.java
+++ src/test/java/org/jboss/xavier/integrations/route/dataformat/CustomizedMultipartDataFormatTest.java
@@ -14,35 +14,35 @@ import java.io.InputStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CustomizedMultipartDataFormatTest {
-
+    
     @Test
     public void customizedMultipartDataFormat_unmarshal_MultipartMessage5FilesGiven_ShouldReturn5Attachments() throws IOException, MessagingException {
         // Given
         CustomizedMultipartDataFormat customizedMultipartDataFormat = new CustomizedMultipartDataFormat();
         Exchange exchange = new DefaultExchange(new DefaultCamelContext());
         exchange.getIn().setHeader("Content-Type", "multipart/text");
-
+        
         InputStream stream = this.getClass().getClassLoader().getResourceAsStream("mime-message-several-files-sample.txt");
-
+        
         // When
         Message message = (Message) customizedMultipartDataFormat.unmarshal(exchange, stream);
-
+        
         // Then
         assertThat(message.getAttachments().size()).isEqualTo(5);
-    }
-
+    }    
+    
     @Test
     public void customizedMultipartDataFormat_unmarshal_TextHtmlMessageGiven_ShouldReturnSameInput() throws IOException, MessagingException {
         // Given
         CustomizedMultipartDataFormat customizedMultipartDataFormat = new CustomizedMultipartDataFormat();
         Exchange exchange = new DefaultExchange(new DefaultCamelContext());
         exchange.getIn().setHeader("Content-Type", "text/html");
-
+        
         InputStream stream = new ByteArrayInputStream("This is a text".getBytes());
-
+        
         // When
         Object message = customizedMultipartDataFormat.unmarshal(exchange, stream);
-
+        
         // Then
         assertThat(message).isEqualTo(stream);
     }
