efc302b16ba7c753d30b8c42700ff5bc113692a0
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-10 11:56:21 +0100

    add docs

--- src/main/java/org/jboss/xavier/integrations/rbac/AclData.java
+++ src/main/java/org/jboss/xavier/integrations/rbac/AclData.java
@@ -2,6 +2,42 @@ package org.jboss.xavier.integrations.rbac;
 
 import java.util.List;
 
+/**
+ * Utility class for storing a single operation and a list of individual resources assigned to the user.
+ * It represents a partial result of processing an ACL and, for instance here {@link RBACService#processAcls(List)}.
+ *
+ * E.g.:
+ * Having {@link RbacResponse}:
+ * {
+ *   "meta": {...},
+ *   "links": {...},
+ *   "data": [
+ *      {
+ *          "permission":"migration-analytics:my-application-resource:read",
+ *          "resourceDefinitions":[
+ *              {
+ *                  "attributeFilter": {
+ *                      "key":"migration-analytics.rh.account",
+ *                      "operation":"in",
+ *                      "value":"123456,654321"
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *   ]
+ * }
+ *
+ * Then we will have this AclData:
+ * {
+ *    "operation": "read",
+ *    "resources":["123456", "654321"]
+ * }
+ *
+ * Which means that the user has read access to the individual resources "123456" and "654321". This result
+ * might be interpret as: User has read access (GET) to '/my-application-resource/123456' and '/my-application-resource/654321'
+ * endpoints, but he has not access to '/my-application-resource/1111'.
+ *
+ */
 public class AclData {
 
     private String operation;
