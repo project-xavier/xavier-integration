b98522dc0d4dadd0f46cb87af811c8e66ec0b600
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-16 18:23:32 +0100

    add cnv as recommended target

--- src/main/java/org/jboss/xavier/analytics/pojo/output/workload/summary/RecommendedTargetsIMSModel.java
+++ src/main/java/org/jboss/xavier/analytics/pojo/output/workload/summary/RecommendedTargetsIMSModel.java
@@ -14,14 +14,22 @@ import javax.persistence.*;
                         @ColumnResult(name = "total", type = Integer.class),
                         @ColumnResult(name = "rhv", type = Integer.class),
                         @ColumnResult(name = "osp", type = Integer.class),
-                        @ColumnResult(name = "rhel", type = Integer.class)
+                        @ColumnResult(name = "rhel", type = Integer.class),
+                        @ColumnResult(name = "cnv", type = Integer.class)
                 }
         )
 )
 
 @NamedNativeQuery(
         name = "RecommendedTargetsIMSModel.calculateRecommendedTargetsIMS",
-        query = "select count(distinct wi.id) as total, coalesce(sum(case when lower(rt.recommended_targetsims)='rhv' then 1 else 0 end), 0) as rhv, coalesce(sum(case when lower(rt.recommended_targetsims)='osp' then 1 else 0 end), 0) as osp, coalesce(sum(case when lower(rt.recommended_targetsims)='rhel' then 1 else 0 end), 0) as rhel from workload_inventory_report_model_recommended_targetsims rt right join workload_inventory_report_model wi on rt.workload_inventory_report_model_id=wi.id where wi.analysis_id = :analysisId",
+        query = "select count(distinct wi.id) as total, \n" +
+                "coalesce(sum(case when lower(rt.recommended_targetsims)='rhv' then 1 else 0 end), 0) as rhv, \n" +
+                "coalesce(sum(case when lower(rt.recommended_targetsims)='osp' then 1 else 0 end), 0) as osp, \n" +
+                "coalesce(sum(case when lower(rt.recommended_targetsims)='rhel' then 1 else 0 end), 0) as rhel, \n" +
+                "coalesce(sum(case when lower(rt.recommended_targetsims)='cnv' then 1 else 0 end), 0) as cnv \n" +
+                "from workload_inventory_report_model_recommended_targetsims rt \n" +
+                "right join workload_inventory_report_model wi on rt.workload_inventory_report_model_id=wi.id \n" +
+                "where wi.analysis_id = :analysisId \n",
         resultSetMapping = "mappingRecommendedTargetsIMSModels"
 )
 
@@ -49,14 +57,16 @@ public class RecommendedTargetsIMSModel
     private Integer rhv;
     private Integer rhel;
     private Integer osp;
+    private Integer cnv;
 
     public RecommendedTargetsIMSModel() {}
 
-    public RecommendedTargetsIMSModel(Integer total, Integer rhv, Integer osp, Integer rhel) {
+    public RecommendedTargetsIMSModel(Integer total, Integer rhv, Integer osp, Integer rhel, Integer cnv) {
         this.total = total;
         this.rhv = rhv;
         this.osp = osp;
         this.rhel = rhel;
+        this.cnv = cnv;
     }
 
     public Long getId() {
@@ -107,6 +117,14 @@ public class RecommendedTargetsIMSModel
         this.osp = osp;
     }
 
+    public Integer getCnv() {
+        return cnv;
+    }
+
+    public void setCnv(Integer cnv) {
+        this.cnv = cnv;
+    }
+
     @Override
     public String toString() {
         return "RecommendedTargetsIMSModel{" +
@@ -115,6 +133,7 @@ public class RecommendedTargetsIMSModel
                 ", rhv='" + rhv +
                 ", rhel=" + rhel +
                 ", osp=" + osp +
+                ", cnv=" + cnv +
                 '}';
     }
 
