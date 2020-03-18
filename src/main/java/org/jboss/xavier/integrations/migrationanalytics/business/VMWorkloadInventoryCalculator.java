934d9448467907d1574f3ab622d6a1e5ed8e500c
m-brophy <mbrophy@redhat.com>
2020-03-18 11:18:10 +0000

    MIGENG-407: changes per review

--- src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/VMWorkloadInventoryCalculator.java
+++ src/main/java/org/jboss/xavier/integrations/migrationanalytics/business/VMWorkloadInventoryCalculator.java
@@ -84,9 +84,9 @@ public class VMWorkloadInventoryCalculator extends AbstractVMWorkloadInventoryCa
         if (hasRdmDisk != null) {
             model.setHasRdmDisk(hasRdmDisk);
         }
-        model.setHasCpuHotAdd(readValueFromExpandedEnvVarPath(CPUHOTADDENABLEDPATH, vmStructMap));
-        model.setHasMemoryHotAdd(readValueFromExpandedEnvVarPath(MEMORYHOTADDENABLEDPATH, vmStructMap));
-        model.setHasCpuHotRemove(readValueFromExpandedEnvVarPath(CPUHOTREMOVEENABLEDPATH, vmStructMap));
+        model.setHasCpuHotAdd(getCpuHotAdd(vmStructMap));
+        model.setHasMemoryHotAdd(getMemoryHotAdd(vmStructMap));
+        model.setHasCpuHotRemove(getCpuHotRemove(vmStructMap));
 
         model.setDiskSpace(getDiskSpaceList(vmStructMap));
 
@@ -107,20 +107,62 @@ public class VMWorkloadInventoryCalculator extends AbstractVMWorkloadInventoryCa
         return model;
     }
 
-    private Long getDiskSpaceList(Map vmStructMap) {
-        // If the VM.used_disk_storage is present use it, if not use VM.DISK[*].size_on_disk
+    private Object getValueForExpandedPathAndHandlePathNotPresent(String path, Map vmStructMap, String errorMessage)
+    {
         try {
-            String usedDiskStoragePath = getExpandedPath(USEDDISKSTORAGEPATH, vmStructMap);
-            Number used_disk_storage = (Number) vmStructMap.get(usedDiskStoragePath);
-            if (used_disk_storage != null) {
-                return used_disk_storage.longValue();
-            }
+            String foundPath = getExpandedPath(path, vmStructMap);
+            Object returnValue = vmStructMap.get(foundPath);
+            if (returnValue!= null)
+                return returnValue;
+
         } catch (Exception e) {
             // In versions previous to 1_0_0 it will fail because there is no such property
-            log.warn("Using an old version of payload. Calculating size with sum of vm.hardware.disks.size_on_disk");
+            log.warn("Using an old version of payload. " + errorMessage);
         }
+        return null;
+    }
+
+    private Long getDiskSpaceList(Map vmStructMap) {
+        // If the VM.used_disk_storage is present use it, if not use VM.DISK[*].size_on_disk
+
+        Object used_disk_storage = getValueForExpandedPathAndHandlePathNotPresent(USEDDISKSTORAGEPATH, vmStructMap,
+                "Calculating size with sum of vm.hardware.disks.size_on_disk");
+        if (used_disk_storage != null) {
+            return ((Number) used_disk_storage).longValue();
+        }
+
 
         List<Number> hardwareDisksList = readListValuesFromExpandedEnvVarPath(DISKSIZEPATH, vmStructMap);
         return hardwareDisksList.stream().filter(Objects::nonNull).mapToLong(Number::longValue).sum();
+    }
+
+    private Boolean getCpuHotAdd(Map vmStructMap) {
+        // If the VM.cpu_hot_add_enabled is present use it, if not set value to null
+
+        Object cpuHotAddPathObject = getValueForExpandedPathAndHandlePathNotPresent(CPUHOTADDENABLEDPATH, vmStructMap,
+                "Setting value to null.");
+
+        return cpuHotAddPathObject != null ? (Boolean) cpuHotAddPathObject : null;
+
+    }
+
+    private Boolean getCpuHotRemove(Map vmStructMap) {
+        // If the VM.cpu_hot_remove_enabled is present use it, if not set value to null
+
+        Object cpuHotRemovePathObject = getValueForExpandedPathAndHandlePathNotPresent(CPUHOTREMOVEENABLEDPATH, vmStructMap,
+                "Setting value to null.");
+
+        return cpuHotRemovePathObject != null ? (Boolean) cpuHotRemovePathObject : null;
+
+    }
+
+    private Boolean getMemoryHotAdd(Map vmStructMap) {
+        // If the VM.memory_hot_add_enabled is present use it, if not set value to null
+
+        Object memoryHotAddPathObject = getValueForExpandedPathAndHandlePathNotPresent(MEMORYHOTADDENABLEDPATH, vmStructMap,
+                "Setting value to null.");
+
+        return memoryHotAddPathObject != null ? (Boolean) memoryHotAddPathObject : null;
+
     }
 }
