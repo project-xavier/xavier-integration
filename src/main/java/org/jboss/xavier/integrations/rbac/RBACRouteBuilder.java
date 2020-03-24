57fb95e871d8c7e0bf1c92ff5f62c6569bbb80aa
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-24 10:07:26 +0100

    add Accept header

--- src/main/java/org/jboss/xavier/integrations/rbac/RBACRouteBuilder.java
+++ src/main/java/org/jboss/xavier/integrations/rbac/RBACRouteBuilder.java
@@ -58,9 +58,9 @@ public class RBACRouteBuilder extends RouteBuilder {
                 .setHeader(RBAC_NEXT_LINK, constant(""))
                 .loopDoWhile(exchange -> exchange.getIn().getHeader(RBAC_NEXT_LINK) != null)
                     .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
-                    .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+                    .setHeader(Exchange.CONTENT_TYPE, constant("application/json")) // Content-type is a way to specify the media type of request being sent from the client to the server (valid on POST and PUT)
+                    .setHeader("Accept").simple("application/json") // Accept header is a way for a client to specify the media type of the response content it is expecting.
                     .setHeader(Exchange.HTTP_PATH, constant(rbacPath))
-                    .setHeader("Accept").simple("application/json") // Force RBAC to response in JSON
                     .process(exchange -> {
                         String httpQuery;
                         String nextLink = exchange.getIn().getHeader(RBAC_NEXT_LINK, String.class);
