fb24db582abd7576309b7e0d4c00af29f49ae95f
jonathan <jvilalop@redhat.com>
2020-05-21 15:33:01 +0200

    MIGENG-623 added Mapper bean to define Date format for JSON

--- src/main/java/org/jboss/xavier/integrations/SpringConfiguration.java
+++ src/main/java/org/jboss/xavier/integrations/SpringConfiguration.java
@@ -1,9 +1,13 @@
 package org.jboss.xavier.integrations;
 
+import java.text.SimpleDateFormat;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +25,21 @@ public class SpringConfiguration {
     @Value("${S3_REGION}")
     String region;
 
+    @Value("${json.dateFormat:yyyy-MM-ddTHH:mm:ss}")
+    String jsonDateFormat;
+
     @Bean(name = "s3client")
     public AmazonS3 getAmazonS3client() {
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(key,secret)))
                 .withRegion(region)
                 .build();
+    }
+
+    @Bean
+    public ObjectMapper gObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat(jsonDateFormat));
+        return mapper;
     }
 }
--- src/test/java/org/jboss/xavier/integrations/TestConfigurationS3.java
+++ src/test/java/org/jboss/xavier/integrations/TestConfigurationS3.java
@@ -1,10 +1,14 @@
 package org.jboss.xavier.integrations;
 
+import java.text.SimpleDateFormat;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +18,9 @@ public class TestConfigurationS3 {
     @Value("${S3_HOST}")
     private String s3_host;
 
+    @Value("${json.dateFormat:yyyy-MM-ddTHH:mm:ss}")
+    String jsonDateFormat;
+
     @Bean(name = "s3client")
     public AmazonS3 getAmazonS3client() {
         return AmazonS3ClientBuilder.standard()
@@ -22,5 +29,10 @@ public class TestConfigurationS3 {
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3_host, "us-east-1"))
                 .build();
     }
-
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat(jsonDateFormat));
+        return mapper;
+    }
 }
