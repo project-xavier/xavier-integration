49a16a907630d838cfea757db1310259e512c78b
jonathan <jvilalop@redhat.com>
2020-06-04 11:06:53 +0200

    MIGENG-622 removed check on limit.max as it will be done from outside on MIGENG-624

--- src/main/java/org/jboss/xavier/integrations/jpa/service/FlagAssessmentService.java
+++ src/main/java/org/jboss/xavier/integrations/jpa/service/FlagAssessmentService.java
@@ -3,7 +3,6 @@ package org.jboss.xavier.integrations.jpa.service;
 import org.jboss.xavier.analytics.pojo.output.workload.summary.FlagAssessmentModel;
 import org.jboss.xavier.integrations.jpa.repository.FlagAssessmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,11 +14,8 @@ public class FlagAssessmentService
     @Autowired
     FlagAssessmentRepository flagAssessmentRepository;
 
-    @Value("${pagination.flag-assessment.limit.max}")
-    int paginationLimitMax;
-
     public Page<FlagAssessmentModel> findAll(int page, int size) {
-        Pageable pageable = new PageRequest(page, (size <= paginationLimitMax) ? size : paginationLimitMax);
+        Pageable pageable = new PageRequest(page, size);
         Page<FlagAssessmentModel> result = flagAssessmentRepository.findAll(pageable);
         return result;
     }
