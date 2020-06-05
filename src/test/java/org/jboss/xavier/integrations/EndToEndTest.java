4e0e0266b6208a99c1ccda2899962937e57f6b8e
jonathan <jvilalop@redhat.com>
2020-06-05 14:54:06 +0200

    MIGENG-622 [PR] fixes of duplication exception added apiservice method to transform pageresponse replaced description of error message

--- src/test/java/org/jboss/xavier/integrations/EndToEndTest.java
+++ src/test/java/org/jboss/xavier/integrations/EndToEndTest.java
@@ -846,6 +846,7 @@ public class EndToEndTest {
         assertThat(initialSavingsEstimationReportService.findByAnalysisOwnerAndAnalysisId("dummy@redhat.com", 3L)).isNull();
         assertThat(getStorageObjectsSize()).isEqualTo(s3ObjectsBefore - 1);
 
+        // Testing that limit and offset params are really taken into consideration
         ResponseEntity<PagedResources<AnalysisModel>> responseAnalysisModel = new RestTemplate().exchange(getBaseURLAPIPath() + "/report?limit=2&offset=0", HttpMethod.GET, getRequestEntity(), new ParameterizedTypeReference<PagedResources<AnalysisModel>>() {});
         assertThat(responseAnalysisModel.getBody().getContent().size()).isEqualTo(2);
 
