da6f43da2a9aee94e60bd747c9d6297d908a7908
Ganesh Hubale <ghubale@redhat.com>
2020-06-17 16:54:03 +0530

    [RFR] Add Jenkins smoke groovy file (#115)
    * Add Jenkins smoke groovy file
    
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>
    
    * Updated ocDeployerServiceSets
    
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

---
+++ Jenkinsfile-smoke.groovy
@@ -0,0 +1,27 @@
+/*
+* Requires: https://github.com/RedHatInsights/insights-pipeline-lib
+*/
+
+@Library("github.com/RedHatInsights/insights-pipeline-lib@v3") _
+
+if (env.CHANGE_ID) {
+    execSmokeTest(
+        // the service-set/component for this app in e2e-deploy "buildfactory"
+        ocDeployerBuilderPath: "xavier/xavier-integration",
+    
+        // the service-set/component for this app in e2e-deploy "templates"
+        ocDeployerComponentPath: "xavier/xavier-integration",
+    
+        // the service sets to deploy into the test environment
+        ocDeployerServiceSets: "platform-mq,rbac,ingress,xavier",
+    
+        // the iqe plugins to install for the test
+        iqePlugins: ["iqe-migration-analytics-plugin"],
+    
+        // the pytest marker to use when calling `iqe tests all`
+        pytestMarker: "ma_smoke",
+    
+        // optional id for an IQE configuration file stored as a secret in Jenkins
+        //configFileCredentialsId: "myId",
+    )
+}
