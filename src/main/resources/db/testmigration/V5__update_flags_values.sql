96b8031869bca661c83fb6a9a1f5fbb4c9125d10
jonathan <jvilalop@redhat.com>
2020-10-06 17:19:53 +0200

    MIGENG-826 created 2 files to update 2 flags

---
+++ src/main/resources/db/testmigration/V5__update_flags_values.sql
@@ -0,0 +1,8 @@
+UPDATE flag_assessment_model
+SET flag_label = 'High Availability (HA) detected' , assessment= 'HA disk locking detected and is unsupported in OpenShift Virtualization'
+WHERE flag = 'VM HA';
+
+UPDATE flag_assessment_model
+SET flag_label = 'CPU affinity detected' , assessment= 'CPU affinity rules detected; unable to assign to specific nodes/CPU''s'
+WHERE flag = 'CPU Affinity';
+
