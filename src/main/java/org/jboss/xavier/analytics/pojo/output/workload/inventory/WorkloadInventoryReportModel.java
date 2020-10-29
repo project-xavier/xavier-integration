d0173ce58fabe898774e7ee757bd7ed5d32bee9c
m-brophy <mbrophy@redhat.com>
2020-10-29 16:17:49 +0000

    MIGENG-854: VM category

--- src/main/java/org/jboss/xavier/analytics/pojo/output/workload/inventory/WorkloadInventoryReportModel.java
+++ src/main/java/org/jboss/xavier/analytics/pojo/output/workload/inventory/WorkloadInventoryReportModel.java
@@ -190,6 +190,8 @@ public class WorkloadInventoryReportModel
 
     private String osFamily;
 
+    private String category;
+
     public WorkloadInventoryReportModel() {}
 
     public static long getSerialVersionUID() {
@@ -388,5 +390,13 @@ public class WorkloadInventoryReportModel
 
     public void setOsFamily(String osFamily) {
         this.osFamily = osFamily;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
