09cfb008b974301d03725712b3566ac7bab469e2
m-brophy <mbrophy@redhat.com>
2020-11-13 15:23:49 +0000

    MIGENG-839: add correct wording for flag

--- src/main/resources/db/migration/V6__sr_iov_flag.sql
+++ src/main/resources/db/migration/V6__sr_iov_flag.sql
@@ -1,2 +1,2 @@
 INSERT INTO flag_assessment_model
-(flag, os_name, assessment, flag_label) VALUES ('SR-IOV Network Interface', '', 'OpenShift Virtualization does not support SR-IOV.', 'SR-IOV network interface detected') ON CONFLICT DO NOTHING;
+(flag, os_name, assessment, flag_label) VALUES ('SR-IOV passthrough adapter', '', 'SR-IOV passthrough adapter configuration is not supported by OpenShift Virtualization.', 'SR-IOV passthrough adapter configuration detected.') ON CONFLICT DO NOTHING;
